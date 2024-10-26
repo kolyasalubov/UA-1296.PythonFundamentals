d054a8228f31c5e58654a0a04ce054d93e8a16b3
Diana Zvierieva <dizvereva1979@fmail.com>
2024-10-26 23:06:55 +0300

    Revert "Merge pull request #7 from kolyasalubov/DianaZvierieva"
    This reverts commit 177d63c768a83ffbf4252da4077d4e974a42f3f7, reversing
    changes made to 929b7f044e83a2c7572231fef04705286a4c4823.

--- HW4/task1HW4.py
+++
@@ -1,8 +0,0 @@
-temperature = float(input("Enter the temperature in Celsius:"))
-
-conversion_to_fahrenheit = (temperature * (9 / 5)) + 32
-
-if temperature < -273.15:
-    print(f"Error: Temperature below absolute zero ({temperature} °C)")
-else:
-    print(f"{temperature} °C is equivalent to {conversion_to_fahrenheit} °F")
