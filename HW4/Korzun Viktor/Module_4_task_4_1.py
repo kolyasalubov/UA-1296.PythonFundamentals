5870f670c555375d5cd57e0380745568ee711a5a
Viktor Korzun <korzunsoft@gmail.com>
2024-10-26 12:28:23 +0300

    Made homework 4

---
+++ HW4/Korzun Viktor/Module_4_task_4_1.py
@@ -0,0 +1,14 @@
+
+
+def checker(valid_number):
+    user_number = int(input("Enter the temperature in  Celsius: "))
+    if valid_number < -273.15:
+        print("Error: Temperature below absolute zero (-273.15)")
+    else:
+        F = (valid_number * 9/5) + 32              
+        print(f"{valid_number}°С is equivalent to {int(F)}°F")
+
+checker(user_number)
+    
+
+
