d017bd326405c6fe4b0b9615d50c6a621ce06a09
Viktor Korzun <korzunsoft@gmail.com>
2024-11-04 15:51:53 +0200

    Make HW 5

---
+++ HW5/KorzunViktor/tasks.py
@@ -0,0 +1,51 @@
+##########    1    ##########  
+
+
+# integer_list = [1,2,3,4,5,6,7,8,9]
+# float_list = []
+
+# for number in integer_list:
+#     number = float(number)
+#     float_list.append(number)
+
+# print(float_list)
+
+
+
+
+##########    2   ########## 
+
+
+# user_number = int(input("Enter the number: "))
+# fibonacci_numbers = [0, 1]
+
+# while True:
+#     fibonacci_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
+#     if fibonacci_number > user_number:
+#         break
+#     fibonacci_numbers.append(fibonacci_number)
+
+# print(fibonacci_numbers)
+
+
+
+
+##########    3   ########## 
+
+
+# user_number = 10
+# number = 1
+
+# for i in range(1,user_number + 1):
+#     number *= i
+
+# print(number)
+
+
+
+
+
+
+
+
+
