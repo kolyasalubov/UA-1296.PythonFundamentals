298a750a9f9f201a38ce1d8d940ab6512cfd4c68
Viktoriia Kyslykh <viktorya.kislyh@gmail.com>
2024-11-04 17:00:08 +0200

    added HW5

---
+++ HW5/KyslykhViktoriia/5.1.py
@@ -0,0 +1,25 @@
+# 5.1
+# a = 10
+# b = 30
+# while a < b:
+#     print(float(a))
+#     a += 1
+
+
+##################################################
+# 5.2
+# n = int(input("Enter an integer number "))
+# a = 0
+# b = 1
+# while a <= n:
+#     print(a)
+#     a, b = b, a + b
+
+
+####################################################
+5.3
+# n = int(input("Enter an integer number "))
+# f = 1
+# for a in range(1, n + 1):
+#     f *= a
+# print(f"Factorial {n} is {f}")
