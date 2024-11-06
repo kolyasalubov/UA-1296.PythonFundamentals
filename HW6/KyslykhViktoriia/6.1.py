dd4782e4199790ec600f7743a5560291992cec6f
Viktoriia Kyslykh <viktorya.kislyh@gmail.com>
2024-11-06 19:29:42 +0200

    added HW6

---
+++ HW6/KyslykhViktoriia/6.1.py
@@ -0,0 +1,16 @@
+# 6.1.1
+# for numbers in range(2, 11, 2):
+#     if numbers % 2 == 0:
+#         print("Even number, which divisible by 2 is", numbers)
+
+
+# 6.1.2
+# for numbers in range(1, 11, 2):
+#     if numbers % 3 == 0:
+#         print("Odd number, which divisible by 3 is", numbers)      
+
+
+# 6.1.3
+# for numbers in range(1, 11):
+#     if numbers % 2 != 0 and numbers % 3 != 0:
+#         print("Not divisible by 2 and 3 is", numbers)
