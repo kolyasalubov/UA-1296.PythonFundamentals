d34885d1d80d03e28722ea07a19b6f0568c93ac4
Diana Zvierieva <dizvereva1979@fmail.com>
2024-11-07 23:14:07 +0200

    added hw7

---
+++ HW7/DianaZvierieva/task1hw7.py
@@ -0,0 +1,12 @@
+def  theLargestNum(num1, num2):
+    """This function takes two numbers and returns more of them."""
+
+    if num1 > num2:
+        print("The greater of two numbers is:", num1) 
+    elif num2 > num1:
+        print("The greater of two numbers is:", num2)
+    else:
+        print("The numbers are equals")   
+
+
+theLargestNum(29, 78)
