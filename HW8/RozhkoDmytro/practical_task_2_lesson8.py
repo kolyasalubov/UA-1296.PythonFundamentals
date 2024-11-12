07b29a5602481c0f64733ec6c7367a39b50948ff
Dmytro Rozhko <d.rozhko.ua@gmail.com>
2024-11-12 14:21:56 +0200

    added task #2, lesson 8

--- HW8/RozhkoDmytro/practical_task_2_lesson8.py
+++ HW8/RozhkoDmytro/practical_task_2_lesson8.py
@@ -1,1 +1,15 @@
+import re
 
+
+def is_valid_password(password: str) -> bool:
+    # truly not my, finded +-pattern in the internet, but I know what's going on
+    pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$#@]).{6,16}$"
+    return bool(re.match(pattern, password))
+
+
+password = input("Enter your password: ")
+
+if is_valid_password(password):
+    print("Password is valid.")
+else:
+    print("Password is not valid.")
