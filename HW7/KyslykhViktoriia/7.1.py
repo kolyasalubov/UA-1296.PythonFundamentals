37817c104b2e89d387ab8cd522e61f73cdba7a46
Viktoriia Kyslykh <viktorya.kislyh@gmail.com>
2024-11-18 10:47:51 +0200

    added HW7

---
+++ HW7/KyslykhViktoriia/7.1.py
@@ -0,0 +1,21 @@
+# num = (7777, 43)
+
+# def numbers():
+#     """
+#     Returns the largest number of two number
+#     """
+#     return max(num)
+
+# max_num = numbers()
+# print("The largest: ", max_num)
+
+
+##############################################
+# def max_num():
+#     """
+#     Returns the largest number of two number
+#     """
+#     return max(7777, 43)
+
+# print("The largest: ", max_num())
+
